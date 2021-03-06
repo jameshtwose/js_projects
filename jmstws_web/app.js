b867b48633ff22a04524df9ebc6633b9a2a5277c
James Twose <jamestwose@Mac-mini.home>
2021-03-06 23:11:06 +0100

    Trying out different versions of the jamestwose.com music website, pretty happy with version 3, just need to clean up how it renders on mobile

--- jmstws_web/app.js
+++ jmstws_web/app.js
@@ -1,7 +1,7 @@
 var menuToggle = document.querySelector("#menu-toggle");
 var activeElements = document.querySelectorAll(".active-element");
-var toggleMenu = document.addEventListener("click", function() {
-  for (var activated =0; activated < activeElements.length; activated += 1){
-    activeElements[activated].classList.toggle("active");
-  }
+var toggledMenu = menuToggle.addEventListener("click", function(){
+    for (var activated = 0; activated < activeElements.length; activated++){
+          activeElements[activated].classList.toggle("active");
+    }
 })
